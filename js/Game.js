d9b9dff0725c8019c515c482e850f3e1349be6af
Kaleb-Moore <k4l3bmoore@gmail.com>
2021-07-04 10:25:01 -0600

    Initial Game Start completed
    Created game and phrase class.
    
    Added new phrases to the game, chose one at random and set that to active.
    
    Parsed active phrase into display method, and output to screen.
    
    Initialized game.

--- js/Game.js
+++ js/Game.js
@@ -1,3 +1,52 @@
 /* Treehouse FSJS Techdegree
  * Project 4 - OOP Game App
  * Game.js */
+
+class Game {
+    constructor () {
+        this.missed = 0;
+        this.phrases = this.createPhrases();
+        this.activePhrase = null;
+    };
+
+    /**
+    * Creates phrases for use in game
+    * @return {array} An array of phrases that could be used in the game
+    */
+    createPhrases() {
+        let phraseList = [];
+        phraseList.push(new Phrase('I know'));
+        phraseList.push(new Phrase('Winner Winner Chicken Dinner'));
+        phraseList.push(new Phrase('You got games on your phone'));
+        phraseList.push(new Phrase('STOP I almost dropped my croissant'));
+        phraseList.push(new Phrase('Ogers are like onions'));
+
+        return phraseList;
+    };
+
+    get randomPhrase() {
+        return this.getRandomPhrase();
+    }
+
+    /**
+    * Selects random phrase from phrases property
+    * @return {Object} Phrase object chosen to be used
+    */
+    getRandomPhrase() {
+        const randomPhrase = Math.floor(Math.random() * this.phrases.length);
+        return this.phrases[randomPhrase];
+    }
+
+    /**
+    * Begins game by selecting a random phrase and displaying it to user
+    */
+    startGame() {
+        const overlay = document.getElementById('overlay');
+        const startBtn = document.getElementById('btn__reset');
+        startBtn.addEventListener('click', e => { 
+            overlay.style.display = 'none';
+        });
+        this.activePhrase = this.randomPhrase;
+        this.randomPhrase.addPhraseToDisplay(this.activePhrase);
+    };
+}
--- js/Phrase.js
+++ js/Phrase.js
@@ -1,3 +1,29 @@
 /* Treehouse FSJS Techdegree
  * Project 4 - OOP Game App
  * Phrase.js */
+
+class Phrase {
+    constructor(phrase){
+        this.phrase = phrase.toLowerCase();
+    }
+
+    /**
+     * Display phrase on game board
+     */
+    addPhraseToDisplay(phrase) {
+        const displayPhrase = document.querySelector('#phrase ul');
+        let letters = phrase.phrase.split('');
+        letters.forEach((letter) => {
+            if(letter === ' '){
+                displayPhrase.insertAdjacentHTML('beforeend', `
+                <li class="space">${letter}</li>
+                `);
+            } else {
+                displayPhrase.insertAdjacentHTML('beforeend', `
+                <li class="hide letter ${letter}">${letter}</li>
+                `);
+            }
+        });
+    }
+
+}
