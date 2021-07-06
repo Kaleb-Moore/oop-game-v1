dd01597fb58c41bf0d23f2bdf3fd481684a90a54
Kaleb-Moore <k4l3bmoore@gmail.com>
2021-07-06 07:55:33 -0600

    Update app.js
    Moved the overlay declarations to the top to set the start screen. Finished the keyup listener by only running its logic if the overlay was displayed to none.
    
    Finished commenting code

--- js/app.js
+++ js/app.js
@@ -6,10 +6,23 @@
  * Listens for any key presses and passes and checks that key
  * press to handeleInteraction if its a valid key.
  */
-    const keys = document.getElementsByClassName('key');
+const keys = document.getElementsByClassName('key');
+
+
+/**
+ * Set start screen background
+ */
+ const overlay = document.getElementById('overlay');
+ overlay.style.backgroundImage = "url('images/Start\ background.jpg')";
+ overlay.style.backgroundRepeat = 'no-repeat';
+ overlay.style.backgroundPosition = 'center';
+ 
+ const title = document.querySelector('h2');
+ title.style.color = "#000000";
 
 /**
  * Calls game start
+ * Handles keyup listener only when the overlay isn't showing
  */
 let game = 
 document.getElementById('btn__reset').addEventListener('click', () => {
@@ -17,14 +30,16 @@ document.getElementById('btn__reset').addEventListener('click', () => {
     game.startGame();
 
     document.addEventListener('keyup', e => {
-        let keyPressed = e.key;
-        for(let i = 0; i < keys.length; i++) {
-            if(keys[i].innerHTML === keyPressed)
-                if(keys[i].disabled) {
-                    continue;
-                } else {
-                    game.handleInteraction(keys[i]);
-                }
+        if(overlay.style.display === 'none') {
+            let keyPressed = e.key;
+            for(let i = 0; i < keys.length; i++) {
+                if(keys[i].innerHTML === keyPressed)
+                    if(keys[i].disabled) {
+                        continue;
+                    } else {
+                        game.handleInteraction(keys[i]);
+                    }
+            }
         }
     });
 
@@ -39,17 +54,3 @@ document.getElementById('qwerty').addEventListener('click', e => {
         game.handleInteraction(e.target)
     }
 });
-
-
-
-/**
- * Set start screen background
- */
-const overlay = document.getElementById('overlay');
-overlay.style.backgroundImage = "url('images/Start\ background.jpg')";
-overlay.style.backgroundRepeat = 'no-repeat';
-overlay.style.backgroundPosition = 'center';
-
-const title = document.querySelector('h2');
-title.style.color = "#000000";
-
